62b12bc51fbe9b72a4e34f6370cc406bb0049011
OsbornDelAngel <ozdelangel@gmail.com>
2022-08-15 20:34:24 -0700

    I have a base file in which calls to the poke api endpoint are happening and names are being populated in the DOM.

---
+++ script.js
@@ -0,0 +1,31 @@
+const API_BASE_URL = 'https://pokeapi.co/api/v2/pokemon/';
+const PAGE_SIZE = 10;
+const pokemonNameContainer = document.getElementById('pokemon-container');
+
+fetchAndAppendPokemon();
+
+function fetchAndAppendPokemon(){
+    const url = pokemonUrlCreator();
+    fetch(url)
+    .then(res => res.json())
+    .then(({results,next }) => {
+        const fragment = document.createDocumentFragment();
+        results.forEach(({name}) => {
+            fragment.appendChild(createPokemonNameElement(name));
+        });
+        pokemonNameContainer.appendChild(fragment);
+    });
+}
+
+function createPokemonNameElement(name) {
+    const pokemonElement = document.createElement('p');
+    pokemonElement.classList.add('pokemon-container');
+    pokemonElement.textContent = name;
+    return pokemonElement
+}
+
+function pokemonUrlCreator(){
+   const url = new URL(API_BASE_URL); 
+   url.searchParams.set('limit', PAGE_SIZE)
+   return url
+}
