c4cbbcbcfe1f0a912dbb80225d35e8317fb7ac0d
OsbornDelAngel <ozdelangel@gmail.com>
2022-08-15 21:00:55 -0700

    the logic for the infinite scroll is there, but there is a problem because the endpoint we are hitting doesnt have an id.

--- script.js
+++ script.js
@@ -2,10 +2,23 @@ const API_BASE_URL = 'https://pokeapi.co/api/v2/pokemon/';
 const PAGE_SIZE = 10;
 const pokemonNameContainer = document.getElementById('pokemon-container');
 
-pokemonNameContainer.addEventListener('scroll', handleScroll);
+let canFetchName = true;
+let afterID = null;
+pokemonNameContainer.addEventListener('scroll', handleScroll());
 fetchAndAppendPokemon();
 
+function handleScroll(){
+    const bottomSpaceLeftToScroll = (
+        this.scrollHeight -
+        this.scrollTop - 
+        this.clientHeight
+    );
+    if (bottomSpaceLeftToScroll > 0) return;
+    fetchAndAppendPokemon();
+}
+
 function fetchAndAppendPokemon(){
+    canFetchName = false;
     const url = pokemonUrlCreator();
     fetch(url)
     .then(res => res.json())
@@ -15,6 +28,12 @@ function fetchAndAppendPokemon(){
             fragment.appendChild(createPokemonNameElement(name));
         });
         pokemonNameContainer.appendChild(fragment);
+        if (next) {
+            afterID = results[results.length - 1].id;
+        }else {
+           pokemonNameContainer.removeEventListener('scroll' , handleScroll) 
+        }
+        canFetchName = true;
     });
 }
 
@@ -28,5 +47,9 @@ function createPokemonNameElement(name) {
 function pokemonUrlCreator(){
    const url = new URL(API_BASE_URL); 
    url.searchParams.set('limit', PAGE_SIZE)
+
+   if (afterID != null){
+    url.searchParams.set('after', afterID)
+   }
    return url
 }
